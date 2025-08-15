137004a43ca366e3bbd3a162ec204a2c892c930f
zosthedev <firstuser0x1@gmail.com>
2025-08-14 22:41:55 -0400

    unlock-termux.sh
    🔐This is only a custom bash script for termux.🔐This script enables wake lock.
    Usage : bash unlock-termux.sh
    
    If the scripts shows : permission denied then Do↓
     Usage : chmod +x unlock-termux.sh 
    
    Future Updates : 📓 custom trap signal script📓

--- unlock-termux.sh
+++ unlock-termux.sh
@@ -1,7 +1,35 @@
-🔐This is only a custom bash script for termux.🔐This script enables wake lock.
-Usage : bash unlock-termux.sh
-
-If the scripts shows : permission denied then Do↓
- Usage : chmod +x unlock-termux.sh 
-
-Future Updates : 📓 custom trap signal script📓
+#usr/bin/bash
+##
+#
+##
+#
+## Colors ##
+#
+##
+#
+BLACK='\e[0;30m'
+BLUE='\e[0;34m'
+GREEN='\e[0;32m'
+CYAN='\e[0;36m'
+RED='\e[0;31m'
+PURPLE='\e[0;35m'
+BROWN='\e[0;33m'
+LIGHTGRAY='\e[0;37m'
+DARKGRAY='\e[1;30m'
+LIGHTBLUE='\e[1;34m'
+LIGHTGREEN='\e[1;32m'
+LIGHTCYAN='\e[1;36m'
+LIGHTRED='\e[1;31m'
+LIGHTPURPLE='\e[1;35m'
+YELLOW='\e[1;33m'
+ULG='\e[4;1;32m'
+WHITE='\e[1;37m'
+NC='\e[0m'
+clear
+termux-wake-unlock
+sleep  1s
+echo -e "${LIGHTGREEN}termux-wake-lock panel"
+echo -e "${WHITE}########################"
+echo -e "${WHITE}#|${YELLOW} ★Developed By zos★${WHITE} |#"
+echo -e "${WHITE}#|${RED} wake lock Disabled${WHITE} |#"
+echo -e "${WHITE}########################"
