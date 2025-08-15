c401428870a1f630beed600883b02da84db57d74
zosthedev <firstuser0x1@gmail.com>
2025-08-14 22:37:09 -0400

    Create unlock-termux.sh
    🔐This is only a custom bash script for termux.🔐This script enables wake lock.
    Usage : bash unlock-termux.sh
    
    If the scripts shows : permission denied then Do↓
     Usage : chmod +x unlock-termux.sh 
    
    Future Updates : 📓 custom trap signal script📓

---
+++ unlock-termux.sh
@@ -0,0 +1,7 @@
+🔐This is only a custom bash script for termux.🔐This script enables wake lock.
+Usage : bash unlock-termux.sh
+
+If the scripts shows : permission denied then Do↓
+ Usage : chmod +x unlock-termux.sh 
+
+Future Updates : 📓 custom trap signal script📓
