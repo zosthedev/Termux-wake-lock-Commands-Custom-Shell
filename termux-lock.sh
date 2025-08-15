a96f452eaf7d089fcf2a59f98723936e0aabcb8a
zosthedev <firstuser0x1@gmail.com>
2025-08-14 22:26:17 -0400

    termux-lock.sh
    🔐This is only a custom bash script for termux.🔐
                      This script enables wake lock.
                        Usage : bash termux-lock.sh
    
          If the scripts shows : permission denied then Do↓
                     Usage : chmod +x termux-lock.sh 
    
    Future Updates : 📓 custom trap signal script📓

---
+++ termux-lock.sh
@@ -0,0 +1,31 @@
+#usr/bin/bash
+##
+#
+## Colors ##
+#
+BLACK='\e[0;30m'
+BLUE='\e[0;34m'
+GREEN='\e[0;32m'
+CYAN='\e[0;36m'
+RED='\e[0;31m'
+PURPLE='\e[0;35m'
+BROWN='\e[0;33m'
+LIGHTGRAY='\e[0;37m'
+DARKGRAY='\e[1;30m'
+LIGHTBLUE='\e[1;34m'
+LIGHTGREEN='\e[1;32m'
+LIGHTCYAN='\e[1;36m'
+LIGHTRED='\e[1;31m'
+LIGHTPURPLE='\e[1;35m'
+YELLOW='\e[1;33m'
+ULG='\e[4;1;32m'
+WHITE='\e[1;37m'
+NC='\e[0m'
+clear
+termux-wake-lock
+sleep  1s
+echo -e "${LIGHTGREEN}termux-wake-lock panel"
+echo -e "${WHITE}########################"
+echo -e "${WHITE}#|${YELLOW} ★Developed By zos★${WHITE} |#"
+echo -e "${WHITE}#|${LIGHTGREEN} wake lock Enabled${WHITE}  |#"
+echo -e "${WHITE}########################"
